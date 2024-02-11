d859412d66ab36b5f74c7d6db1f19d16cba14ad0
aswin7469 <aswinas@pixysos.com>
2024-02-12 01:32:29 +0530

    thermal: cleanup and setup sepolicy
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- thermal/device.mk
+++
@@ -1,8 +0,0 @@
-PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel-floral
-
-# Thermal utils
-PRODUCT_PACKAGES += \
-    thermal_symlinks
-
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/thermal
